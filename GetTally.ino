eaa9f037e6a32cfbdc91a14e81dc93715cb37303
Anthony Thompson <anthonythompson@me.com>
2019-07-04 14:04:04 -0500

    Add files via upload

--- GetTally.ino
+++ GetTally.ino
@@ -11,45 +11,33 @@
    This was created for a dedicated LED box at each camera to provide the URSA Mini 4Ks in our studios
    an "onboard" tally light for use with Autoscript prompter optosensors. Tally Override is not enabled
    so tally will be passed through the shield to the BMD camera.
+
+   created 03 July 2019
+   by Anthony Thompson
+   modified 04 July 2019
+   by Anthony Thompson 
 */
 
 // includes the Blackmagic Design SDI Control library.
 #include <BMDSDIControl.h>
 
-// declares a class object called 'tally' using the default I²C address
-BMD_SDITallyControl_I2C tally(0x6E);
+BMD_SDITallyControl_I2C tally(0x6E);  // declares Tally Control object using the default I²C address
+byte data[128];                       // Tally Data length is 128 bytes
+int atemID = 7;                       // ATEM ID of the camera
 
-// Tally Data length is 128 bytes, so array of bytes was created
-byte data[128];
-
-// ATEM ID of the camera this module is defined here
-int atemID = 4;
-
-
-// standard Arduino setup function
 void setup() {
-
-  // begins tally control and does not override incoming tally
-  tally.begin();
-  tally.setOverride(false);
-
-  // configures pin 8 as an output.
-  pinMode(8, OUTPUT);
+  tally.begin();                      // begins tally control
+  tally.setOverride(false);           // does not override incoming tally
+  pinMode(8, OUTPUT);                 // configures pin 8 as an output
 }
 
-// standard Arduino looping function
 void loop() {
+  tally.read(data);                   // takes in an array and fills it with the data in the ITDATA register
 
-  // tally control read function that takes in an array and fills it with the data in the ITDATA register
-  tally.read(data);
-
-  // simple logic that compares camera ATEM ID to a corresponding zero-based entry in the data array
-  if ( data[atemID - 1] == 1 ) {
-    digitalWrite(8, HIGH);
-  } else {
-    digitalWrite(8, LOW);
-  }
-
-  // wait for 33 milliseconds or just shy of 1 frame at 29.97 or 30 fps
-  delay(33);
+  if ( data[atemID - 1] == 1 ) {      // simple logic that compares camera ATEM ID to entry in the data array
+    digitalWrite(8, HIGH); }
+  else {
+    digitalWrite(8, LOW); }
+  
+  delay(33);                          // wait for 33 milliseconds or just shy of 1 frame at 29.97 or 30 fps
 }
